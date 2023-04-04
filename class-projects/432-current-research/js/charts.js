68e9f7bd99588e5f3626929284e7fb33f4109e8e
cjst3ch <cjstech@tamu.edu>
2023-04-04 15:22:18 -0500

    testing charts

--- class-projects/432-current-research/js/charts.js
+++ class-projects/432-current-research/js/charts.js
@@ -1,9 +1,10 @@
 // Load google charts
 google.charts.load('current', { 'packages': ['corechart'] });
-google.charts.setOnLoadCallback(chartOneName);
+google.charts.setOnLoadCallback(ChartOneName);
+google.charts.setOnLoadCallback(ChartTwoName);
 
 // Draw the chart and set the chart values
-function chartOneName() {
+function ChartOneName() {
     var data = google.visualization.arrayToDataTable([
         ['Task', 'Number of Games'],
         ['Work', 8],
@@ -18,5 +19,23 @@ function chartOneName() {
 
     // Display the chart inside the <div> element with id="piechart"
     var chart = new google.visualization.BarChart(document.getElementById('chartOneName'));
+    chart.draw(data, options);
+}
+
+function ChartTwoName() {
+    var data = google.visualization.arrayToDataTable([
+        ['Task', 'Testing'],
+        ['Work', 8],
+        ['Eat', 2],
+        ['TV', 4],
+        ['Gym', 2],
+        ['Sleep', 8]
+    ]);
+
+    // Optional; add a title and set the width and height of the chart
+    var options = { 'title': 'My Average Day', 'width': 550, 'height': 400 };
+
+    // Display the chart inside the <div> element with id="piechart"
+    var chart = new google.visualization.BarChart(document.getElementById('chartTwoName'));
     chart.draw(data, options);
 }
