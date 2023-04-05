619931c04f8acc8e15d0bb5db30fffd3758844ce
cjst3ch <cjstech@tamu.edu>
2023-04-04 19:30:57 -0500

    Add Category of Games Charts

--- class-projects/432-current-research/js/charts.js
+++ class-projects/432-current-research/js/charts.js
@@ -1,6 +1,6 @@
 // Load google charts
 google.charts.load('current', { 'packages': ['corechart'] });
-google.charts.setOnLoadCallback(ChartOneName);
+google.charts.setOnLoadCallback(CategoryOfGame);
 google.charts.setOnLoadCallback(ChartTwoName);
 google.charts.setOnLoadCallback(ChartThreeName);
 google.charts.setOnLoadCallback(ChartFourName);
@@ -19,21 +19,20 @@ google.charts.setOnLoadCallback(ChartSixteenName);
 google.charts.setOnLoadCallback(ChartSeventeenName);
 
 // Draw the chart and set the chart values
-function ChartOneName() {
+function CategoryOfGame() {
     var data = google.visualization.arrayToDataTable([
-        ['Task', 'Number of Games'],
-        ['Work', 8],
-        ['Eat', 2],
-        ['TV', 4],
-        ['Gym', 2],
-        ['Sleep', 8]
+        ['Category', 'Number of Games'],
+        ['Puzzle', 11],
+        ['Shooter', 1],
+        ['Adventure', 3],
+        ['Racing', 3]
     ]);
 
     // Optional; add a title and set the width and height of the chart
-    var options = { 'title': 'My Average Day', 'width': 550, 'height': 400 };
+    var options = { 'title': 'Category of the Games (Overlap)', 'width': 550, 'height': 400 };
 
     // Display the chart inside the <div> element with id="piechart"
-    var chart = new google.visualization.BarChart(document.getElementById('ChartOneName'));
+    var chart = new google.visualization.BarChart(document.getElementById('CategoryOfGame'));
     chart.draw(data, options);
 }
 
